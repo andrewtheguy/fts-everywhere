b40c3e36cebc43afb9a2eb9820d38e6c4701b879
andrew <hello@andrewtheguy.com>
2026-05-25 02:45:40 +0000

    Fix response message in root route from "ok2" to "ok"

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "ok2" }));
+    let app = Router::new().route("/", get(|| async { "ok" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on http://localhost:3000");
